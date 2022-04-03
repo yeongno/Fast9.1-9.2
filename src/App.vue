b28207565779b5e5e469ea06cfda5047ebd163a0
yeongno <dldudsh83@gmail.com>
2022-04-04 01:59:26 +0900

    9.1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h1 @click="increase">
+  {{count}}
+  </h1>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      count: 0
+    }
+  },
+  methods:{
+    increase(){
+      this.count += 1
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  font-size: 50px;
+  color: royalblue;
+}
+</style>
