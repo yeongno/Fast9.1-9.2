2a4b2b568e1e80aef01bb0f567c7d56afbab18f0
yeongno <dldudsh83@gmail.com>
2022-04-04 23:59:44 +0900

    템플릿 문법

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <h1>{{ count }}</h1>
+  <!-- <h1 v-bind:class="msg">{{ msg }}</h1> -->
+  <h1 :[attr]="'active'"
+  @[event]="add">{{ msg }}</h1>
 </template>
 
 <script>
 export default {
   data(){
     return{
-      count: 2
+      msg: 'active',
+      attr: 'class',
+      event: 'click'
     }
   },
-  beforeCreate(){//데이터가 정의 되기 전임으로 undefined
-    console.log('Before Create!', this.count)
-  },
-  created(){//html과 연결이 되기 전임으로 null
-    console.log('Created!', this.count)
-    console.log(document.querySelector('h1'))
-  },
-  beforeMount(){//html과 연결이 되기 전임으로 null
-    console.log('Before Mount!')
-    console.log(document.querySelector('h1'))
-  },
-  mounted(){//화면에 있는 내용을 조회하여 사용하기 위해선 mounted를 사용
-    console.log('Mounted!')
-    console.log(document.querySelector('h1'))
+  methods: {
+    add(){
+      this.msg += '!'
+    }
   }
 }
 </script>
+
+<style scoped>
+.active{
+  color:royalblue;
+  font-size: 100px;
+}
+</style>
